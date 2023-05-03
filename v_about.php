fae0e4f7ee6643a426dc66d8d5b78b6d55c723b4
nadiaaprilian <128135594+nadiaaprilian@users.noreply.github.com>
2023-05-03 10:41:02 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+    <li>Nama Depan : Nadia Aprilia</li>
+    <li>Nama Belakang : Ningrum</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li>JL Asrama Polri Cipinang </li>
+  </ul>
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>Jakarta 21/04/2002</li>
+  </ul>
+  <h4>Olah Raga Favorit</h4>
+  <ul type="square">
+    <li>VolleyBall</li>
+    <li>Badminton</li>
+  </ul>
+</section>
